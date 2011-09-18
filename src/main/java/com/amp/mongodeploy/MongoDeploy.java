8dbbf8293e0a02bb226e8691ae189ce5f04c9db9
Andre mougeot <mougeot.andre@gmail.com>
2011-09-18 13:13:01 -0600

    Starting to add support for errors to be reported

--- src/main/java/com/amp/mongodeploy/MongoDeploy.java
+++ src/main/java/com/amp/mongodeploy/MongoDeploy.java
@@ -5,41 +5,57 @@ import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 
+import java.util.Date;
 import java.util.List;
 
 public class MongoDeploy {
+    public static final String MONGO_DEPLOY_COLLECTION = "mongoDeploy";
+    protected static final String ID_FIELD = "_id";
+    protected static final String DATE_FIELD = "date";
 
-    public static final String ID_FIELD = "_id";
     private DB mongoDb;
     private List<MongoDeployScript> mongoDeployScripts;
-    public static final String MONGO_DEPLOY_COLLECTION = "mongoDeploy";
+    private boolean hasError = false;
 
     public MongoDeploy(DB mongoDb, List<MongoDeployScript> mongoDeployScripts) {
         this.mongoDb = mongoDb;
         this.mongoDeployScripts = mongoDeployScripts;
     }
 
+    public boolean hasError() {
+        return hasError;
+    }
+
     public void runScripts() {
         for(MongoDeployScript script : mongoDeployScripts) {
-            if (scriptHasNotBeenRun(script)) {
-                script.run();
-                trackScript(script);
+            try {
+                runScript(script);
+            } catch (Exception e) {
+                hasError = true;
             }
+        }
+    }
+
+    private void runScript(MongoDeployScript script) {
+        if (scriptHasNotBeenRun(script)) {
+            script.run();
+            trackScript(script);
         }
     }
 
     private void trackScript(MongoDeployScript script) {
         DBObject scriptRecord = dbObject(ID_FIELD, script.getScriptName());
+        scriptRecord.put(DATE_FIELD, new Date());
         getCollection().save(scriptRecord);
     }
 
     private boolean scriptHasNotBeenRun(MongoDeployScript script) {
         DBObject scriptQuery = dbObject(ID_FIELD, script.getScriptName());
         DBObject scriptInDatabase = getCollection().findOne(scriptQuery);
-        
+
         return scriptInDatabase == null;
     }
-
     private DBCollection getCollection() { return mongoDb.getCollection(MONGO_DEPLOY_COLLECTION); }
+
     private DBObject dbObject(String key, Object value) { return new BasicDBObject(key, value); }
 }
--- src/test/java/com/amp/mongodeploy/MongoDeployTest.java
+++ src/test/java/com/amp/mongodeploy/MongoDeployTest.java
@@ -1,22 +1,27 @@
 package com.amp.mongodeploy;
 
+import com.mongodb.DBObject;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.mockito.InOrder;
 
+import java.util.Arrays;
 import java.util.Collections;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class MongoDeployTest extends MongoTest {
 
     MongoDeploy mongoDeploy;
+    MongoDeployScript mockScript;
 
 
     @Before
     public void setup() {
-        
+        mockScript = mock(MongoDeployScript.class);
+        mongoDeploy = new MongoDeploy(mongoDb, Collections.singletonList(mockScript));
     }
 
     @After
@@ -26,10 +31,7 @@ public class MongoDeployTest extends MongoTest {
 
     @Test
     public void runScripts_shouldRunTheGivenScript_whenItHasNotBeenRun() {
-        MongoDeployScript mockScript = mock(MongoDeployScript.class);
-
         //execute
-        mongoDeploy = new MongoDeploy(mongoDb, Collections.singletonList(mockScript));
         mongoDeploy.runScripts();
 
         //assert
@@ -38,15 +40,72 @@ public class MongoDeployTest extends MongoTest {
 
     @Test
     public void runScripts_shouldNotRunTheGivenScript_whenItHasAlreadyBeenRun() {
-        MongoDeployScript mockScript = mock(MongoDeployScript.class);
         when(mockScript.getScriptName()).thenReturn("ClassName");
 
         //execute
-        mongoDeploy = new MongoDeploy(mongoDb, Collections.singletonList(mockScript));
         mongoDeploy.runScripts();
         mongoDeploy.runScripts();
 
         //assert
         verify(mockScript, times(1)).run();
+    }
+
+    @Test
+    public void runScripts_shouldRegisterTheIdInTheCollection_whenTheScriptIsRun() {
+        String expectedScriptId = "ClassName";
+        when(mockScript.getScriptName()).thenReturn(expectedScriptId);
+
+        //execute
+        mongoDeploy.runScripts();
+
+        //assert
+        DBObject result = mongoDb.getCollection(MongoDeploy.MONGO_DEPLOY_COLLECTION).findOne();
+        assertEquals(expectedScriptId, result.get("_id"));
+    }
+
+    @Test
+    public void runScripts_shouldRegisterTheRunDateInTheCollection_whenTheScriptIsRun() {
+        when(mockScript.getScriptName()).thenReturn("ClassName");
+
+        //execute
+        mongoDeploy.runScripts();
+
+        //assert
+        DBObject result = mongoDb.getCollection(MongoDeploy.MONGO_DEPLOY_COLLECTION).findOne();
+        assertNotNull(result.get("date"));
+    }
+
+    @Test
+    public void hasError_shouldReturnTrue_whenAnErrorOccursWhenAScriptThrowsAnError() {
+        doThrow(new RuntimeException()).when(mockScript).run();
+
+        //execute
+        mongoDeploy.runScripts();
+
+        //assert
+        assertTrue(mongoDeploy.hasError());
+    }
+
+    @Test
+    public void hasError_shouldReturnFalse_whenTheScriptRunsNormally() {
+        //execute
+        mongoDeploy.runScripts();
+
+        //assert
+        assertFalse(mongoDeploy.hasError());
+    }
+
+    @Test
+    public void runScripts_shouldRunAllScripts_whenMultipleScriptsAreProvided() {
+        MongoDeployScript mockScript2 = mock(MongoDeployScript.class);
+        mongoDeploy = new MongoDeploy(mongoDb, Arrays.asList(mockScript, mockScript2));
+
+        //execute
+        mongoDeploy.runScripts();
+
+        //assert
+        InOrder inOrder = inOrder(mockScript, mockScript2);
+        inOrder.verify(mockScript).run();
+        inOrder.verify(mockScript2).run();
     }
 }
